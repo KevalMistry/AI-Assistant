189fe03bd74434dd49ec2aee4ef75696cda384b5
KevalMistry <mistrykeval2004@gmail.com>
2025-04-03 19:07:26 +0530

    Updated with flask

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello I am  you AI Assistant")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! Your AI Assistant is running on Railway 🚀"
+
+if __name__ == "__main__":
+    app.run(debug=True)
